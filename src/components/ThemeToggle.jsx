d69f541a6ea26f27a208e12c69d3057f0620dcf4
Sai Manikanta Madhavarapu <madhavarapu.saimanikanta@gmail.com>
2025-09-06 12:43:03 +0530

    updated and added new files

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -28,7 +29,12 @@ export const ThemeToggle = () => {
     };
 
     return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme} 
+            className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden"
+        )} >
+
         {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-300" />}
     </button>);
 }
