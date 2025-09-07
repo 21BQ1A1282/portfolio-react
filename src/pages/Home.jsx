875fda2afe9c89c437363e77629d3694c6d405d2
Sai Manikanta Madhavarapu <madhavarapu.saimanikanta@gmail.com>
2025-09-07 18:22:50 +0530

    files updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
-import { StarBackground } from "../components/StarBackground";
-import { ThemeToggle } from "../components/ThemeToggle";
+import { HeroSection } from "@/components/HeroSection";
+import { Navbar } from "@/components/Navbar";
+import { StarBackground } from "@/components/StarBackground";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { AboutSection } from "@/components/AboutSection";
 
 export const Home = () => {
     return(
@@ -12,8 +15,13 @@ export const Home = () => {
         <StarBackground />
 
         {/* Navbar */}
+        <Navbar />
 
         {/* Main Content */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+        </main>
 
         {/* Footer */}
 
